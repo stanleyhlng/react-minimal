af7473fa8042c7774079b869dce6ff1cf2cf3d32
stanleyn <stanleyhlng@yahoo.com.hk>
2016-06-29 23:15:05 -0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+var config = {
+    context: path.join(__dirname, 'src'),
+    entry: [
+        './main.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'www'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ['babel']
+            }
+        ]
+    },
+    resolveLoader: {
+        root: [
+            path.join(__dirname, 'node_modules')
+        ]
+    },
+    resolve: {
+        root: [
+            path.join('__dirname', 'node_modules')
+        ]
+    }
+};
+
+module.export = config;
