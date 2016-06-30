f9bfaef07a856bde1099e833ccc0d566b709bb99
stanleyn <stanleyhlng@yahoo.com.hk>
2016-06-30 00:32:47 -0700

    add react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 console.log('Hello World!');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+ReactDOM.render(
+    <Counter />,
+    document.getElementById('mount')
+);
